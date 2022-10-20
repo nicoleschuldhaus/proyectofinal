b08bc4d7b6cc11872993b8adfd0616eed46d3a7c
Nicole Schuldhaus <115422528+nicoleschuldhaus@users.noreply.github.com>
2022-10-20 11:58:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let idCounter = 0;
+const input = document.querySelector('input[type="text"]');
+
+userInput.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    addTask();
+});
+
+let addTask = ()=>{
+    idCounter++;
+
+    let newValue = input.value;
+
+    list.innerHTML += `
+    <div class="task-container" id="${idCounter}">
+        <label>
+            <input type="checkbox">
+            ${newValue}
+        </label>
+        <img src="./images/borrar.png" alt="basura" class="closeBtn">
+    </div>
+    `
+
+    input.value = '';
+    updateStats();
+};
+
+list.addEventListener('click',(event)=>{
+    if(event.srcElement.nodeName == 'INPUT'){
+        updateStats();
+    } else if(event.srcElement.nodeName == 'IMG'){
+        deleteTask(event.srcElement.parentNode.id);
+    }
+});
+
+let updateStats = ()=>{
+    let element = list.querySelectorAll('div');
+    let checkbox = list.querySelectorAll('input[type="checkbox"]:checked') 
+    stats.innerHTML = `<p>Tareas Pendientes: ${element.length} | Completadas: ${checkbox.length} </p>`
+};
+
+let deleteTask = (id)=>{
+    let taskToDelete = document.getElementById(id);
+    list.removeChild (taskToDelete);
+    updateStats();
+}
+
